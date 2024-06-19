782f116ff973a998505186236d8948299cff3b7c
Yashvardhan Toshniwal <120038916+YAshT10@users.noreply.github.com>
2024-06-20 02:06:23 +0530

    Organised

--- Autologin/autologin.py
+++ Autologin/autologin.py
@@ -3,12 +3,15 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Edge()
-
+print("Enter your Username")
+usr=input()
+print("Enter your Password")
+pwd=input()
 driver.get('http://netaccess.iitm.ac.in/account/login')
 username = driver.find_element(By.ID, 'username')
-username.send_keys('')#username
+username.send_keys(usr)#username
 password = driver.find_element(By.ID, 'password')
-password.send_keys('')#passcode
+password.send_keys(pwd)#passcode
 password.submit()
 driver.get('https://netaccess.iitm.ac.in/account/approve')
 button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/form/div[2]/label/input")
